a252e5bf1f7953d118fa5a1802b2c6fb0a308740
Arthur Metekohy <a.metekohy@hotmail.com>
2021-02-26 14:01:31 +0100

    implemented two synchronous GET-requests to github.com & github readme.

--- src/main/java/com/ametekohy/app/services/GitHubService.java
+++ src/main/java/com/ametekohy/app/services/GitHubService.java
@@ -1,12 +1,45 @@
 package com.ametekohy.app.services;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class GitHubService {
+    private HttpClient httpClient;
+
+    public GitHubService() {
+        httpClient = HttpClient.newHttpClient();
+    }
 
     public String getIndexHtml() {
-        return "";
+        var request = HttpRequest.newBuilder().uri(URI.create("https://github.com")).build();
+
+        HttpResponse<String> response = null;
+
+        try {
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return response.body();
     }
 
     public String getReadme() {
-        return "";
+        var request = HttpRequest.newBuilder().uri(URI.create("https://raw.githubusercontent.com/ametekohy/HTTP-ClientAPI/master/README.md")).build();
+
+        HttpResponse<String> response = null;
+
+        try {
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return response.body();
     }
 }
